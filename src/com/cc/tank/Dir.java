6cbd90caac884f2c04a496a779700469e4f12566
zhaoxin <1837997910@qq.com>
2020-10-26 15:19:38 +0800

    第四次提交，设置方向常量，判断按下键怎么移动

---
+++ src/com/cc/tank/Dir.java
@@ -0,0 +1,5 @@
+package com.cc.tank;
+
+public enum Dir {
+	LEFT,RIGHT,UP,DOWN;
+}
--- src/com/cc/tank/TankFrame.java
+++ src/com/cc/tank/TankFrame.java
@@ -11,6 +11,8 @@ public class TankFrame extends Frame{
 	
 	int x=200;
 	int y=200;
+	Dir dir=Dir.DOWN;
+	private static final int speed=10;
 	
 	public TankFrame() {
 		setSize(800, 600);//大小
@@ -31,23 +33,92 @@ public class TankFrame extends Frame{
 	@Override
 	public void paint(Graphics g){//只要窗口需要重新绘制就被自动调用，会清理之前的显示
 		g.fillRect(x, y, 50, 50);//填充矩形
-		x+=10;
+		//x+=10;
 		//y+=10;
+		switch (dir) {
+		case LEFT:
+			x-=speed;
+			break;
+		case RIGHT:
+			x+=speed;
+			break;
+		case UP:
+			y-=speed;
+			break;
+		case DOWN:
+			y+=speed;
+			break;
+		default:
+			break;
+		}
 		
 	}
 	
 	class MyKeyListener extends KeyAdapter{
+		
+		boolean bU=false;//表示哪个方向键被按下
+		boolean bD=false;
+		boolean bL=false;
+		boolean bR=false;
 
 		@Override
 		public void keyPressed(KeyEvent e) {//按下键盘
-			System.out.println("key pressed");
-			x+=30;
-			//repaint();
+			int key=e.getKeyCode();
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				//x-=10;
+				bL=true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				//x+=10;
+				bR=true;
+				break;
+			case KeyEvent.VK_UP:
+				//y-=10;
+				bU=true;
+				break;
+			case KeyEvent.VK_DOWN:
+				//y+=10;
+				bD=true;
+				break;
+
+			default:
+				break;
+			}
+			
+			setMainTankDir();
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {//释放键盘
-			System.out.println("key released");
+			int key=e.getKeyCode();
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				bL=false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=false;
+				break;
+			case KeyEvent.VK_UP:
+				bU=false;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=false;
+				break;
+
+			default:
+				break;
+			}
+			
+			setMainTankDir();
+		}
+
+		private void setMainTankDir() {
+			if(bL) dir=Dir.LEFT;
+			if(bR) dir=Dir.RIGHT;
+			if(bU) dir=Dir.UP;
+			if(bD) dir=Dir.DOWN;
+			
 		}
 		
 	}

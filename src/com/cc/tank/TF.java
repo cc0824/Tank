e4ed5c8e38d2592b9e85e71843040d468567b9d2
zhaoxin <1837997910@qq.com>
2020-10-26 09:23:00 +0800

    第二次提交，将窗口坐标改为变量

---
+++ src/com/cc/tank/TF.java
@@ -0,0 +1,16 @@
+package com.cc.tank;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TF {
+
+	public static void main(String[] args) {
+		TankFrame f=new TankFrame();
+		
+		
+		
+
+	}
+
+}
--- src/com/cc/tank/TankFrame.java
+++ src/com/cc/tank/TankFrame.java
@@ -1,27 +1,36 @@
 package com.cc.tank;
 
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class TankFrame {
-
-	public static void main(String[] args) {
-		Frame f=new Frame();
-		f.setSize(800, 600);//大小
-		f.setResizable(false);//大小不可变
-		f.setTitle("tank war");//标题
-		f.setVisible(true);//打开窗口
+public class TankFrame extends Frame{
+	
+	int x=200;
+	int y=200;
+	
+	public TankFrame() {
+		setSize(800, 600);//大小
+		setResizable(false);//大小不可变
+		setTitle("tank war");//标题
+		setVisible(true);//打开窗口
 		
-		f.addWindowListener(new WindowAdapter() {//监听器点击x关闭窗口
+		addWindowListener(new WindowAdapter() {//监听器点击x关闭窗口
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
-		
-		
-
 	}
+	
+	@Override
+	public void paint(Graphics g){//只要窗口需要重新绘制就被自动调用，会清理之前的显示
+		g.fillRect(x, y, 50, 50);//填充矩形
+		x+=10;
+		y+=10;
+		
+	}
+	
 
 }
